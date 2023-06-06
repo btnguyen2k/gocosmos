5c904807d4c9bf7bbb34882f892234bbe39df963
Thanh Nguyen <thanhn@microsoft.com>
2023-06-06 23:29:24 +1000

    refactor UPSERT and update tests

--- gocosmos_test.go
+++ gocosmos_test.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"os"
-	"regexp"
 	"strings"
 	"testing"
-	"time"
 )
 
 func Test_OpenDatabase(t *testing.T) {
@@ -127,124 +125,6 @@ func TestDriver_Close(t *testing.T) {
 }
 
 /*----------------------------------------------------------------------*/
-
-func Test_Query_Upsert(t *testing.T) {
-	name := "Test_Query_Upsert"
-	db := _openDb(t, name)
-	_, err := db.Query("UPSERT INTO db.table (a,b,c) VALUES (1,2,3)", nil)
-	if err == nil || strings.Index(err.Error(), "not supported") < 0 {
-		t.Fatalf("%s failed: expected 'not support' error, but received %#v", name, err)
-	}
-}
-
-func Test_Exec_Upsert(t *testing.T) {
-	name := "Test_Exec_Upsert"
-	db := _openDb(t, name)
-
-	db.Exec("DROP DATABASE IF EXISTS db_not_exists")
-	db.Exec("DROP DATABASE IF EXISTS dbtemp")
-	db.Exec("CREATE DATABASE IF NOT EXISTS dbtemp")
-	defer db.Exec("DROP DATABASE IF EXISTS dbtemp")
-	if _, err := db.Exec("CREATE COLLECTION dbtemp.tbltemp WITH pk=/username WITH uk=/email"); err != nil {
-		t.Fatalf("%s failed: %s", name, err)
-	}
-
-	if result, err := db.Exec(`UPSERT INTO dbtemp.tbltemp (id, username, email, grade, actived) VALUES ("\"1\"", "\"user1\"", "\"user1@domain.com\"", 7, true)`, "user1"); err != nil {
-		t.Fatalf("%s failed: %s", name, err)
-	} else if id, err := result.LastInsertId(); id != 0 || err == nil {
-		t.Fatalf("%s failed: expected LastInsertId=0/err!=nil but received LastInsertId=%d/err=%s", name, id, err)
-	} else if regexp.MustCompile(`(?i){\s*LastInsertId\s*:\s*[^}]+?\s*}`).FindString(err.Error()) == "" {
-		t.Fatalf("%s failed: can not catch LastInsertId / %s", name, err)
-	} else if numRows, err := result.RowsAffected(); numRows != 1 || err != nil {
-		t.Fatalf("%s failed: expected RowsAffected=1/err=nil but received RowsAffected=%d/err=%s", name, numRows, err)
-	}
-	if result, err := db.Exec(`UPSERT INTO dbtemp.tbltemp (id, username, email, grade, actived) VALUES ("\"2\"", "\"user2\"", "\"user2@domain.com\"", 7, true)`, "user2"); err != nil {
-		t.Fatalf("%s failed: %s", name, err)
-	} else if id, err := result.LastInsertId(); id != 0 || err == nil {
-		t.Fatalf("%s failed: expected LastInsertId=0/err!=nil but received LastInsertId=%d/err=%s", name, id, err)
-	} else if regexp.MustCompile(`(?i){\s*LastInsertId\s*:\s*[^}]+?\s*}`).FindString(err.Error()) == "" {
-		t.Fatalf("%s failed: can not catch LastInsertId / %s", name, err)
-	} else if numRows, err := result.RowsAffected(); numRows != 1 || err != nil {
-		t.Fatalf("%s failed: expected RowsAffected=1/err=nil but received RowsAffected=%d/err=%s", name, numRows, err)
-	}
-
-	if result, err := db.Exec(`UPSERT INTO dbtemp.tbltemp (id,username,email,grade,actived) VALUES ("\"1\"", "\"user1\"", "\"user1@domain1.com\"", 8, false)`, "user1"); err != nil {
-		// duplicated id (in logical partition scope): existing document should be overwritten
-		t.Fatalf("%s failed: %s", name, err)
-	} else if id, err := result.LastInsertId(); id != 0 || err == nil {
-		t.Fatalf("%s failed: expected LastInsertId=0/err!=nil but received LastInsertId=%d/err=%s", name, id, err)
-	} else if regexp.MustCompile(`(?i){\s*LastInsertId\s*:\s*[^}]+?\s*}`).FindString(err.Error()) == "" {
-		t.Fatalf("%s failed: can not catch LastInsertId / %s", name, err)
-	} else if numRows, err := result.RowsAffected(); numRows != 1 || err != nil {
-		t.Fatalf("%s failed: expected RowsAffected=1/err=nil but received RowsAffected=%d/err=%s", name, numRows, err)
-	}
-
-	if _, err := db.Exec(`UPSERT INTO dbtemp.tbltemp (id,username,email,grade,actived) VALUES ("\"1\"", "\"user2\"", "\"user2@domain.com\"", 9, true)`, "user2"); err != ErrConflict {
-		// duplicated unique index (in logical partition scope)
-		t.Fatalf("%s failed: expected ErrConflict but received %#v", name, err)
-	}
-
-	if _, err := db.Exec(`UPSERT INTO db_not_exists.table (id,username,email) VALUES ("\"x\"", "\"y\"", "\"x\"")`, "y"); err != ErrNotFound {
-		// database/table not found
-		t.Fatalf("%s failed: expected ErrNotFound but received %#v", name, err)
-	}
-	if _, err := db.Exec(`UPSERT INTO dbtemp.tbl_not_found (id,username,email) VALUES ("\"x\"", "\"y\"", "\"x\"")`, "y"); err != ErrNotFound {
-		// database/table not found
-		t.Fatalf("%s failed: expected ErrNotFound but received %#v", name, err)
-	}
-}
-
-func Test_Exec_UpsertPlaceholder(t *testing.T) {
-	name := "Test_Exec_UpsertPlaceholder"
-	db := _openDb(t, name)
-
-	db.Exec("DROP DATABASE IF EXISTS db_not_exists")
-	db.Exec("DROP DATABASE IF EXISTS dbtemp")
-	db.Exec("CREATE DATABASE IF NOT EXISTS dbtemp")
-	defer db.Exec("DROP DATABASE IF EXISTS dbtemp")
-	if _, err := db.Exec("CREATE COLLECTION dbtemp.tbltemp WITH pk=/username WITH uk=/email"); err != nil {
-		t.Fatalf("%s failed: %s", name, err)
-	}
-
-	if result, err := db.Exec(`UPSERT INTO dbtemp.tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`,
-		"1", "user1", "user1@domain.com", 1, true, map[string]interface{}{"str": "a string", "num": 1.23, "bool": true, "date": time.Now()}, "user1"); err != nil {
-		t.Fatalf("%s failed: %s", name, err)
-	} else if id, err := result.LastInsertId(); id != 0 || err == nil {
-		t.Fatalf("%s failed: expected LastInsertId=0/err!=nil but received LastInsertId=%d/err=%s", name, id, err)
-	} else if regexp.MustCompile(`(?i){\s*LastInsertId\s*:\s*[^}]+?\s*}`).FindString(err.Error()) == "" {
-		t.Fatalf("%s failed: can not catch LastInsertId / %s", name, err)
-	} else if numRows, err := result.RowsAffected(); numRows != 1 || err != nil {
-		t.Fatalf("%s failed: expected RowsAffected=1/err=nil but received RowsAffected=%d/err=%s", name, numRows, err)
-	}
-	if result, err := db.Exec(`UPSERT INTO dbtemp.tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`,
-		"2", "user2", "user2@domain.com", 2, false, map[string]interface{}{"str": "a string", "num": 1.23, "bool": true, "date": time.Now()}, "user2"); err != nil {
-		t.Fatalf("%s failed: %s", name, err)
-	} else if id, err := result.LastInsertId(); id != 0 || err == nil {
-		t.Fatalf("%s failed: expected LastInsertId=0/err!=nil but received LastInsertId=%d/err=%s", name, id, err)
-	} else if regexp.MustCompile(`(?i){\s*LastInsertId\s*:\s*[^}]+?\s*}`).FindString(err.Error()) == "" {
-		t.Fatalf("%s failed: can not catch LastInsertId / %s", name, err)
-	} else if numRows, err := result.RowsAffected(); numRows != 1 || err != nil {
-		t.Fatalf("%s failed: expected RowsAffected=1/err=nil but received RowsAffected=%d/err=%s", name, numRows, err)
-	}
-
-	if result, err := db.Exec(`UPSERT INTO dbtemp.tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`,
-		"1", "user1", "user2@domain.com", 2, false, nil, "user1"); err != nil {
-		// duplicated id (in logical partition scope): existing document should be overwritten
-		t.Fatalf("%s failed: %s", name, err)
-	} else if id, err := result.LastInsertId(); id != 0 || err == nil {
-		t.Fatalf("%s failed: expected LastInsertId=0/err!=nil but received LastInsertId=%d/err=%s", name, id, err)
-	} else if regexp.MustCompile(`(?i){\s*LastInsertId\s*:\s*[^}]+?\s*}`).FindString(err.Error()) == "" {
-		t.Fatalf("%s failed: can not catch LastInsertId / %s", name, err)
-	} else if numRows, err := result.RowsAffected(); numRows != 1 || err != nil {
-		t.Fatalf("%s failed: expected RowsAffected=1/err=nil but received RowsAffected=%d/err=%s", name, numRows, err)
-	}
-
-	if _, err := db.Exec(`UPSERT INTO dbtemp.tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`,
-		"2", "user1", "user2@domain.com", 3, false, nil, "user1"); err != ErrConflict {
-		// duplicated unique index (in logical partition scope)
-		t.Fatalf("%s failed: expected ErrConflict but received %#v", name, err)
-	}
-}
 
 func Test_Query_Delete(t *testing.T) {
 	name := "Test_Query_Delete"
--- stmt_document_test.go
+++ stmt_document_test.go
@@ -292,3 +292,314 @@ func TestStmtInsert_Exec_DefaultDb(t *testing.T) {
 		})
 	}
 }
+
+func TestStmtUpsert_Query(t *testing.T) {
+	testName := "TestStmtUpsert_Query"
+	db := _openDb(t, testName)
+	_, err := db.Query("UPSERT INTO db.table (a,b,c) VALUES (1,2,3)", nil)
+	if err != ErrQueryNotSupported {
+		t.Fatalf("%s failed: expected ErrQueryNotSupported, but received %#v", testName, err)
+	}
+}
+
+func TestStmtUpsert_Exec(t *testing.T) {
+	testName := "TestStmtUpsert_Exec"
+	db := _openDb(t, testName)
+	dbname := "dbtemp"
+	defer db.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname))
+	testData := []struct {
+		name         string
+		initSqls     []string
+		sql          string
+		args         []interface{}
+		mustConflict bool
+		mustNotFound bool
+		mustError    string
+		affectedRows int64
+	}{
+		{
+			name: "upsert_new",
+			initSqls: []string{
+				"DROP DATABASE IF EXISTS db_not_exists",
+				fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname),
+				fmt.Sprintf("CREATE DATABASE %s", dbname),
+				fmt.Sprintf("CREATE COLLECTION %s.tbltemp WITH pk=/username WITH UK=/email", dbname),
+			},
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbltemp (id, username, email, grade, actived) VALUES ("\"1\"", "\"user1\"", "\"user1@domain.com\"", 7, true)`, dbname),
+			args:         []interface{}{"user1"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_another",
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbltemp (id, username, email, grade, actived) VALUES ("\"2\"", "\"user2\"", "\"user2@domain.com\"", 7, true)`, dbname),
+			args:         []interface{}{"user2"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_duplicated_id_placeholders",
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbltemp (id,username,email,grade,actived) VALUES ("\"1\"", "\"user1\"", "\"user3@domain1.com\"", 8, false)`, dbname),
+			args:         []interface{}{"user1"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_conflict_uk",
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbltemp (id,username,email,grade,actived) VALUES ("\"3\"", "\"user2\"", "\"user2@domain.com\"", 9, true)`, dbname),
+			args:         []interface{}{"user2"},
+			mustConflict: true,
+		},
+		{
+			name:         "table_not_exists",
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbl_not_found (id,username,email) VALUES ("\"x\"", "\"y\"", "\"x\"")`, dbname),
+			args:         []interface{}{"y"},
+			mustNotFound: true,
+		},
+		{
+			name:         "db_not_exists",
+			sql:          `UPSERT INTO db_not_exists.table (id,username,email) VALUES ("\"x\"", "\"y\"", "\"x\"")`,
+			args:         []interface{}{"y"},
+			mustNotFound: true,
+		},
+		{
+			name: "upsert_new_placeholders",
+			initSqls: []string{
+				"DROP DATABASE IF EXISTS db_not_exists",
+				fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname),
+				fmt.Sprintf("CREATE DATABASE %s", dbname),
+				fmt.Sprintf("CREATE COLLECTION %s.tbltemp WITH pk=/username WITH uk=/email", dbname),
+			},
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`, dbname),
+			args:         []interface{}{"1", "user1", "user1@domain.com", 1, true, map[string]interface{}{"str": "a string", "num": 1.23, "bool": true, "date": time.Now()}, "user1"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_another_placeholders",
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`, dbname),
+			args:         []interface{}{"2", "user2", "user2@domain.com", 2, false, map[string]interface{}{"str": "a string", "num": 1.23, "bool": true, "date": time.Now()}, "user2"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_duplicated_id_placeholders",
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`, dbname),
+			args:         []interface{}{"1", "user1", "user2@domain.com", 2, false, nil, "user1"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_conflict_uk_placeholders",
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`, dbname),
+			args:         []interface{}{"2", "user1", "user2@domain.com", 3, false, nil, "user1"},
+			mustConflict: true,
+		},
+		{
+			name:         "table_not_exists_placeholders",
+			sql:          fmt.Sprintf(`UPSERT INTO %s.tbl_not_found (id,username,email) VALUES (:1, :2, :3)`, dbname),
+			args:         []interface{}{"x", "y", "x", "y"},
+			mustNotFound: true,
+		},
+		{
+			name:         "db_not_exists_placeholders",
+			sql:          `UPSERT INTO db_not_exists.table (id,username,email) VALUES (@1, @2, @3)`,
+			args:         []interface{}{"x", "y", "x", "y"},
+			mustNotFound: true,
+		},
+		{
+			name:      "error_invalid_value_index",
+			sql:       fmt.Sprintf(`UPSERT INTO %s.tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :10)`, dbname),
+			args:      []interface{}{"2", "user", "user@domain1.com", 3, false, nil, "user"},
+			mustError: "invalid value index",
+		},
+	}
+	for _, testCase := range testData {
+		t.Run(testCase.name, func(t *testing.T) {
+			for _, initSql := range testCase.initSqls {
+				_, err := db.Exec(initSql)
+				if err != nil {
+					t.Fatalf("%s failed: {error: %s / sql: %s}", testName+"/"+testCase.name+"/init", err, initSql)
+				}
+			}
+			execResult, err := db.Exec(testCase.sql, testCase.args...)
+			if testCase.mustConflict && err != ErrConflict {
+				t.Fatalf("%s failed: expect ErrConflict but received %#v", testName+"/"+testCase.name+"/exec", err)
+			}
+			if testCase.mustNotFound && err != ErrNotFound {
+				t.Fatalf("%s failed: expect ErrNotFound but received %#v", testName+"/"+testCase.name+"/exec", err)
+			}
+			if testCase.mustConflict || testCase.mustNotFound {
+				return
+			}
+			if testCase.mustError != "" {
+				if err == nil || strings.Index(err.Error(), testCase.mustError) < 0 {
+					t.Fatalf("%s failed: expected '%s' bur received %#v", testCase.name, testCase.mustError, err)
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("%s failed: %s", testName+"/"+testCase.name+"/exec", err)
+			}
+
+			affectedRows, err := execResult.RowsAffected()
+			if err != nil {
+				t.Fatalf("%s failed: %s", testName+"/"+testCase.name+"/rows_affected", err)
+			}
+			if affectedRows != testCase.affectedRows {
+				t.Fatalf("%s failed: expected %#v affected-rows but received %#v", testName+"/"+testCase.name, testCase.affectedRows, affectedRows)
+			}
+			_, err = execResult.LastInsertId()
+			if err == nil {
+				t.Fatalf("%s failed: expected LastInsertId but received nil", testName+"/"+testCase.name)
+			}
+			lastInsertId := make(map[string]interface{})
+			err = json.Unmarshal([]byte(err.Error()), &lastInsertId)
+			if err != nil {
+				t.Fatalf("%s failed: %s", testName+"/"+testCase.name+"/LastInsertId", err)
+			}
+			if len(lastInsertId) != 1 {
+				t.Fatalf("%s failed - LastInsertId: %#v", testName+"/"+testCase.name+"/LastInsertId", lastInsertId)
+			}
+		})
+	}
+}
+
+func TestStmtUpsert_Exec_DefaultDb(t *testing.T) {
+	testName := "TestStmtUpsert_Exec_DefaultDb"
+	dbname := "dbdefault"
+	db := _openDefaultDb(t, testName, dbname)
+	defer db.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname))
+	testData := []struct {
+		name         string
+		initSqls     []string
+		sql          string
+		args         []interface{}
+		mustConflict bool
+		mustNotFound bool
+		mustError    string
+		affectedRows int64
+	}{
+		{
+			name: "upsert_new",
+			initSqls: []string{
+				"DROP DATABASE IF EXISTS db_not_exists",
+				fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname),
+				fmt.Sprintf("CREATE DATABASE %s", dbname),
+				fmt.Sprintf("CREATE COLLECTION %s.tbltemp WITH pk=/username WITH UK=/email", dbname),
+			},
+			sql:          `UPSERT INTO tbltemp (id, username, email, grade, actived) VALUES ("\"1\"", "\"user1\"", "\"user1@domain.com\"", 7, true)`,
+			args:         []interface{}{"user1"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_another",
+			sql:          `UPSERT INTO tbltemp (id, username, email, grade, actived) VALUES ("\"2\"", "\"user2\"", "\"user2@domain.com\"", 7, true)`,
+			args:         []interface{}{"user2"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_duplicated_id_placeholders",
+			sql:          `UPSERT INTO tbltemp (id,username,email,grade,actived) VALUES ("\"1\"", "\"user1\"", "\"user3@domain1.com\"", 8, false)`,
+			args:         []interface{}{"user1"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_conflict_uk",
+			sql:          `UPSERT INTO tbltemp (id,username,email,grade,actived) VALUES ("\"3\"", "\"user2\"", "\"user2@domain.com\"", 9, true)`,
+			args:         []interface{}{"user2"},
+			mustConflict: true,
+		},
+		{
+			name:         "table_not_exists",
+			sql:          `UPSERT INTO tbl_not_found (id,username,email) VALUES ("\"x\"", "\"y\"", "\"x\"")`,
+			args:         []interface{}{"y"},
+			mustNotFound: true,
+		},
+		{
+			name: "upsert_new_placeholders",
+			initSqls: []string{
+				"DROP DATABASE IF EXISTS db_not_exists",
+				fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname),
+				fmt.Sprintf("CREATE DATABASE %s", dbname),
+				fmt.Sprintf("CREATE COLLECTION %s.tbltemp WITH pk=/username WITH uk=/email", dbname),
+			},
+			sql:          `UPSERT INTO tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`,
+			args:         []interface{}{"1", "user1", "user1@domain.com", 1, true, map[string]interface{}{"str": "a string", "num": 1.23, "bool": true, "date": time.Now()}, "user1"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_another_placeholders",
+			sql:          `UPSERT INTO tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`,
+			args:         []interface{}{"2", "user2", "user2@domain.com", 2, false, map[string]interface{}{"str": "a string", "num": 1.23, "bool": true, "date": time.Now()}, "user2"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_duplicated_id_placeholders",
+			sql:          `UPSERT INTO tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`,
+			args:         []interface{}{"1", "user1", "user2@domain.com", 2, false, nil, "user1"},
+			affectedRows: 1,
+		},
+		{
+			name:         "upsert_conflict_uk_placeholders",
+			sql:          `UPSERT INTO tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :6)`,
+			args:         []interface{}{"2", "user1", "user2@domain.com", 3, false, nil, "user1"},
+			mustConflict: true,
+		},
+		{
+			name:         "table_not_exists_placeholders",
+			sql:          `UPSERT INTO tbl_not_found (id,username,email) VALUES (:1, :2, :3)`,
+			args:         []interface{}{"x", "y", "x", "y"},
+			mustNotFound: true,
+		},
+		{
+			name:      "error_invalid_value_index",
+			sql:       `UPSERT INTO tbltemp (id, username, email, grade, actived, data) VALUES (:1, $2, @3, @4, $5, :10)`,
+			args:      []interface{}{"2", "user", "user@domain1.com", 3, false, nil, "user"},
+			mustError: "invalid value index",
+		},
+	}
+	for _, testCase := range testData {
+		t.Run(testCase.name, func(t *testing.T) {
+			for _, initSql := range testCase.initSqls {
+				_, err := db.Exec(initSql)
+				if err != nil {
+					t.Fatalf("%s failed: {error: %s / sql: %s}", testName+"/"+testCase.name+"/init", err, initSql)
+				}
+			}
+			execResult, err := db.Exec(testCase.sql, testCase.args...)
+			if testCase.mustConflict && err != ErrConflict {
+				t.Fatalf("%s failed: expect ErrConflict but received %#v", testName+"/"+testCase.name+"/exec", err)
+			}
+			if testCase.mustNotFound && err != ErrNotFound {
+				t.Fatalf("%s failed: expect ErrNotFound but received %#v", testName+"/"+testCase.name+"/exec", err)
+			}
+			if testCase.mustConflict || testCase.mustNotFound {
+				return
+			}
+			if testCase.mustError != "" {
+				if err == nil || strings.Index(err.Error(), testCase.mustError) < 0 {
+					t.Fatalf("%s failed: expected '%s' bur received %#v", testCase.name, testCase.mustError, err)
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("%s failed: %s", testName+"/"+testCase.name+"/exec", err)
+			}
+
+			affectedRows, err := execResult.RowsAffected()
+			if err != nil {
+				t.Fatalf("%s failed: %s", testName+"/"+testCase.name+"/rows_affected", err)
+			}
+			if affectedRows != testCase.affectedRows {
+				t.Fatalf("%s failed: expected %#v affected-rows but received %#v", testName+"/"+testCase.name, testCase.affectedRows, affectedRows)
+			}
+			_, err = execResult.LastInsertId()
+			if err == nil {
+				t.Fatalf("%s failed: expected LastInsertId but received nil", testName+"/"+testCase.name)
+			}
+			lastInsertId := make(map[string]interface{})
+			err = json.Unmarshal([]byte(err.Error()), &lastInsertId)
+			if err != nil {
+				t.Fatalf("%s failed: %s", testName+"/"+testCase.name+"/LastInsertId", err)
+			}
+			if len(lastInsertId) != 1 {
+				t.Fatalf("%s failed - LastInsertId: %#v", testName+"/"+testCase.name+"/LastInsertId", lastInsertId)
+			}
+		})
+	}
+}
